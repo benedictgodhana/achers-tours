73d3a342a9e664f3396ec980cb74bb456a15daa1
benedictgodhana <benedictgodhana7@gmail.com>
2024-12-18 10:53:48 +0300

    new

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -169,13 +169,8 @@ s0.parentNode.insertBefore(s1,s0);
   <div class="container">
     <div class="row no-gutters slider-text align-items-center justify-content-center">
       <div class="col-md-9 ftco-animate pb-5 text-center">
-        <p class="breadcrumbs">
-          <span class="mr-2">
-            <a href="index.html">Home <i class="fa fa-chevron-right"></i></a>
-          </span>
-          <span>{{ $blog->title }} <i class="fa fa-chevron-right"></i></span>
-        </p>
-        <h1 class="mb-0 bread">{{ $blog->title }}</h1>
+
+        <h1 class="mb-0 bread" style="font-size:16px">{{ $blog->title }}</h1>
         <p class="text-center" style="font-size:12px;font-weight:800">
           By {{ $blog->author }} on {{ $blog->created_at->format('M d, Y') }}
         </p>
